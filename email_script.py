964f34ca85f6773548f7f9c9cbfbddfecd6f85a2
Kha <khnguyen26@gmail.com>
2020-08-19 23:22:46 -0400

    outlining steps

--- email_script.py
+++ email_script.py
@@ -11,12 +11,25 @@ port = 465
 sender_email = ''
 username = input('username: ')
 password = input('password: ')
-message = ['hello', 'hello2']
+message = ['hello', 'hello2'] # Will be null, type string
 
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(username, password)
-    for i in range(0, len(receiver_emails)):
-        server.sendmail(sender_email, receiver_emails[i], message[i])
-        print('Email Sent')
+
+    for i in range(0, len(df.index)):
+        # Send 2025 Mentee 1 email about their Mentor
+            # Format Message
+
+        # Send 2023 Mentor email about their Mentee 1
+
+        # Check if there is a Mentee 2 for Mentor
+        if(not (pd.isnull(df.loc[i, 'Email_2']))):
+            # Send 2025 Mentee 2 email about their Mentor
+
+            # Send 2023 Mentor email about their Mentee 2
+
+    # for i in range(0, len(receiver_emails)):
+    #     server.sendmail(sender_email, receiver_emails[i], message[i])
+    #     print('Email Sent')
 
