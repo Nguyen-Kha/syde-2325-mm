d96fedd7484f1ea6943908944dee466366950f2c
Kha <khnguyen26@gmail.com>
2020-08-22 22:00:35 -0400

    functional

--- email_script.py
+++ email_script.py
@@ -3,10 +3,15 @@ import pandas as pd
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-df = pd.read_excel('')
+# def send_23_email(i):
+
+# def send_25_1_email(i):
+
+# def send_25_2_email(i):
+
+df = pd.read_excel('csv/test_match.xlsx')
 
 smtp_server = 'mailservices.uwaterloo.ca'
-receiver_emails = ['', '']
 
 port = 465
 # port = 587
@@ -18,62 +23,94 @@ context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(username, password)
 
-    # for i in range(0, len(df.index)):
-    #     # Send 2025 Mentee 1 email about their Mentor
-    #         # Format Message
+    for i in range(0, len(df.index)):
+        # message_to23 = MIMEMultipart()
+        # message_to23['From'] = sender_email
+        # message_to23['To'] = df['2023_Email'][i]
+        # message_to23['Subject'] = 'Your SYDE 23/25 Mentor/Mentee Pairing!'
+        # message_to23["Bcc"] = sender_email
 
-    #     # Send 2023 Mentor email about their Mentee 1
+        message_to25_1 = MIMEMultipart()
+        message_to25_1['From'] = sender_email
+        # message_to25_1['To'] = df['2023_Email'][i]
+        # message_to25_1['To'] = df['Email_1'][i]
+        message_to25_1['Subject'] = 'Your SYDE 23/25 Mentor/Mentee Pairing!'
+        message_to25_1["Bcc"] = sender_email
 
-    #     # Check if there is a Mentee 2 for Mentor
-    #     if(not (pd.isnull(df.loc[i, 'Email_2']))):
-    #         # Send 2025 Mentee 2 email about their Mentor
+        message_to25_2 = MIMEMultipart()
+        message_to25_2['From'] = sender_email
+        # message_to25_2['To'] = df['2023_Email'][i]
+        # message_to25_2['To'] = df['Email_2'][i]
+        message_to25_2['Subject'] = 'Your SYDE 23/25 Mentor/Mentee Pairing!'
+        message_to25_2["Bcc"] = sender_email
 
-    #         # Send 2023 Mentor email about their Mentee 2
 
-    # for i in range(0, len(receiver_emails)):
-    message_to23 = MIMEMultipart()
-    message_to23['From'] = sender_email
-    message_to23['To'] = df['2023_Email'][2]
-    message_to23['Subject'] = 'Your SYDE 23/25 Mentor/Mentee Pairing!'
+        ###   EMAILING THE 2023s   ###
+        # If a 2023 has a single 2025
+        if(pd.isnull(df.loc[i, '2025_2'])):
+            message_contents = ''
+            message_contents = 'Hi ' + (df[2023][i]).split()[0] + ',\n\n'
+            message_contents = message_contents + 'Thank you for signing up to be a mentor. We appreciate you reaching out to the 2025s and '
+            message_contents = message_contents + 'helping them feel more at ease with their first year of university. We have paired you with your mentee, '
+            message_contents = message_contents + df['2025_1'][i] + '!\n\n'
 
-    # message_to25_1 = MIMEMultipart()
-    # message_to25_1['From'] = sender_email
-    # message_to25_1['To'] = sender_email
-    # message_to25_1['Subject'] = 'Your SYDE 23/25 Mentor/Mentee Pairing!'
+            message_contents = message_contents + 'Here is what ' + (df['2025_1'][i]).split()[0] + ' had to say about themselves: \n' + df['Bio_1'][i] + '\n'
+            message_contents = message_contents + 'You can reach out to them at: ' + df['Email_1'][i] + '.\n\n'
 
-    # message_to25_2 = MIMEMultipart()
-    # message_to25_2['From'] = sender_email
-    # message_to25_2['To'] = sender_email
-    # message_to25_2['Subject'] = 'Your SYDE 23/25 Mentor/Mentee Pairing!'
+            message_contents = message_contents + 'Please email your mentees in the next couple of days. If you have any questions, reach out to me. '
+            message_contents = message_contents + 'Thanks again for volunteering to be a mentor!\n\n'
 
-    # message_to23["Bcc"] = sender_email
+        # If a 2023 has 2 Mentees
+        else:
+            message_contents = ''
+            message_contents = 'Hi ' + (df[2023][i]).split()[0] + ',\n\n'
+            message_contents = message_contents + 'Thank you for signing up to be a mentor. We appreciate you reaching out to the 2025s and '
+            message_contents = message_contents + 'helping them feel more at ease with their first year of university. We have paired you with 2 mentees: '
+            message_contents = message_contents + df['2025_1'][i] + ' and ' + df['2025_2'][i] + '!\n\n'
 
-    ###   EMAILING THE 2023s   ###
-    # If a 2023 has a single 2025
-    if(pd.isnull(df.loc[2, '2025_2'])):
-        message_contents = 'Hi ' + (df[2023][36]).split()[0] + ',\n\nYour Mentee is ' + df['2025_1'][36] + '. Here is what they said: \n\n'
-        message_contents = message_contents + df['Bio_1'][36] + '\n\nThanks for participating. Send me any questions.\n\n'
-        message_contents = message_contents + 'Best, \nKha Nguyen'
+            message_contents = message_contents + 'Here is what ' + (df['2025_1'][i]).split()[0] + ' had to say about themselves: \n' + df['Bio_1'][i] + '\n'
+            message_contents = message_contents + 'You can reach out to them at: ' + df['Email_1'][i] + '.\n\n'
+            message_contents = message_contents + 'Here is what ' + (df['2025_2'][i]).split()[0] + ' had to say about themselves: \n' + df['Bio_2'][i] + '\n'
+            message_contents = message_contents + 'You can reach out to them at: ' + df['Email_2'][i] + '.\n\n'
 
-    # If a 2023 has 2 Mentees
-    else:
-        message_contents = 'Hi ' + (df[2023][2]).split()[0] + ',\n'
-        message_contents = message_contents + 'Thank you for signing up to be a mentor. We appreciate you reaching out to the 2025s and '
-        message_contents = message_contents + 'helping them feel more at ease with their first year of university. We have paired you 2 mentees: '
-        message_contents = message_contents + df['2025_1'][2] + ' and ' + df['2025_2'][2] + '!\n\n'
+            message_contents = message_contents + 'Please email your mentees in the next couple of days. If you have any questions, reach out to me. '
+            message_contents = message_contents + 'Thanks again for volunteering to be a mentor!\n\n'
+            
 
-        message_contents = message_contents + 'Here is what ' + df['2025_1'][2] + ' had to say about themselves: \n' + df['Bio_1'][2] + '\n'
-        message_contents = message_contents + 'You can reach out to them at: ' + df['Email_1'][2] + '.\n\n'
-        message_contents = message_contents + 'Here is what ' + df['2025_2'][2] + ' had to say about themselves: \n' + df['Bio_2'][2] + '\n'
-        message_contents = message_contents + 'You can reach out to them at: ' + df['Email_2'][2] + '.\n\n'
+        message_to23.attach(MIMEText(message_contents, 'plain'))
+        message_to23 = message_to23.as_string()
+        server.sendmail(sender_email, df['2023_Email'][i], message_to23) 
+        print('Email Sent To 2023')
 
-        message_contents = message_contents + 'Please email your mentees in the next couple of days. If you have any questions, reach out to me. '
-        message_contents = message_contents + 'Thanks again for volunteering to be a mentor!\n\n'
-        message_contents = message_contents + 'Best,\nKha'
+        ######   EMAILING THE 2025s   ######
+        ###   2025_1   ###
+        message_contents = ''
+        message_contents = 'Hi ' + (df['2025_1'][i]).split()[0] + ',\n\n'
+        message_contents = message_contents + 'Thank you for signing up to be a mentee. Your first year of university may be challenging but we\'re '
+        message_contents = message_contents + 'here to help you through it! We have paired you with your mentor, ' + df[2023][i] + '.\n\n'
+        message_contents = message_contents + 'Here is what ' + (df[2023][i]).split()[0] + ' had to say about themselves: \n' + df['2023_Bio'][i] + '\n\n'
+        message_contents = message_contents + 'Your mentor will reach out to you within a week from now. If it has been a week and your mentor has not reached '
+        message_contents = message_contents + 'out to you, or if you have any questions or concerns, please email me at kha.nguyen@uwaterloo.ca.'
         
+        message_to25_1.attach(MIMEText(message_contents, 'plain'))
+        message_to25_1 = message_to25_1.as_string()
+        # server.sendmail(sender_email, df['Email_1'][i], message_to25_1)
+        # server.sendmail(sender_email, df['2023_Email'][i], message_to25_1) 
+        print('Email Sent To 2025_1')
 
-    message_to23.attach(MIMEText(message_contents, 'plain'))
-    message_to23 = message_to23.as_string()
-    server.sendmail(sender_email, sender_email, message_to23)
-    print('Email Sent')
+        ###   2025_2   ###
+        if(not (pd.isnull(df.loc[i, '2025_2']))):
+            message_contents = ''
+            message_contents = 'Hi ' + (df['2025_2'][i]).split()[0] + ',\n\n'
+            message_contents = message_contents + 'Thank you for signing up to be a mentee. Your first year of university may be challenging but we\'re '
+            message_contents = message_contents + 'here to help you through it! We have paired you with your mentor, ' + df[2023][i] + '.\n\n'
+            message_contents = message_contents + 'Here is what ' + (df[2023][i]).split()[0] + ' had to say about themselves: \n' + df['2023_Bio'][i] + '\n\n'
+            message_contents = message_contents + 'Your mentor will reach out to you within a week from now. If it has been a week and your mentor has not reached '
+            message_contents = message_contents + 'out to you, or if you have any questions or concerns, please email me at kha.nguyen@uwaterloo.ca.'
+        
+            message_to25_2.attach(MIMEText(message_contents, 'plain'))
+            message_to25_2 = message_to25_2.as_string()
+            # server.sendmail(sender_email, df['Email_2'][i], message_to25_2)
+            # server.sendmail(sender_email, df['2023_Email'][i], message_to25_2) 
+            print('Email Sent To 2025_2')
 
